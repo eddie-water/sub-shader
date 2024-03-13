b8dbf372ee9fc765c9a908a4b6c47593e399c2e3
Eddie Devlin <edevlin64@gmail.com>
2024-03-12 23:30:19 -0400

    No plotting in the model

--- models/file_operations.py
+++ models/file_operations.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
 
 class FileOperations:
     def __init__(self, file_path: str) -> None:
@@ -11,12 +9,6 @@ class FileOperations:
         length = self.data.shape[0] / self.sample_rate
 
         time = np.linspace(0., length, self.data.shape[0])
-        plt.plot(time, self.data[:, 0], label="Left channel")
-        plt.plot(time, self.data[:, 1], label="Right channel")
-        plt.legend()
-        plt.xlabel("Time [s]")
-        plt.ylabel("Amplitude")
-        plt.show()
 
         print("Reading", self.file_path) 
         print(self.file_path, "has", self.num_channels, "channels")
