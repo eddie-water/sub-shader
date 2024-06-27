ad971d9a997baccb3d1bbab4273edf70111a1961
Eddie Devlin <edevlin64@gmail.com>
2024-06-26 21:04:03 -0400

    Clean up wave play

--- models/wave_play.py
+++ models/wave_play.py
@@ -2,22 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pywt
 
-def create_mixed_signal(time, sinusoid_list, length, sample_rate):
-    sample_period = 1.0 / sample_rate
+def create_mixed_signal(time, sinusoid_list):
 
     num_samples = time.size
-    num_sinusoids = len(sinusoid_list)
 
-    print("num_sinuspoids", num_sinusoids)
-    signal = np.zeros(shape = (num_sinusoids, num_samples))
-    print(signal)
+    signal = np.zeros(num_samples)
 
-    # TODO delete this 
-    i = 0 
     for start, end, freq in sinusoid_list:
-
-        print(start, end, freq)
-
         n = time
 
         # Create impulse using start and end %
@@ -32,20 +23,13 @@ def create_mixed_signal(time, sinusoid_list, length, sample_rate):
             else:
                 mask.append(0)
         mask = np.array(mask)
-        print("mask:", mask)
 
         # Apply mask
         n = n * mask
 
-        signal[i] += np.sin(2*np.pi*freq*n)
+        signal += np.sin(2*np.pi*freq*n)
 
-        fig, ax = plt.subplots()
-        ax.set_title("Mask of " + str(freq) + " Hz Signal")
-        ax.plot(t, signal[i])
-
-        i += 1
-
-    return (signal[0] + signal[1] + signal[2])
+    return signal
 
 # TODO moves this to example file
 if __name__ == "__main__":
@@ -53,7 +37,7 @@ if __name__ == "__main__":
     # The audio was sampled at 44100 Hz
     SAMPLING_FREQUENCY = 44100
     SAMPLING_PERIOD = 1.0 / SAMPLING_FREQUENCY
-    print("The Sampling Frequency:", SAMPLING_FREQUENCY, "Hz and Sampling Period", SAMPLING_PERIOD, "seconds")
+    # print("The Sampling Frequency:", SAMPLING_FREQUENCY, "Hz and Sampling Period", SAMPLING_PERIOD, "seconds")
 
     # How long the audio is in seconds
     AUDIO_LENGTH_SECONDS = 1
@@ -70,11 +54,10 @@ if __name__ == "__main__":
     # print("Linspace: Error:", abs(SAMPLING_PERIOD - ta_sample_period))
     # print("Arange: The average sampling period is:", tb_sample_period)
     # print("Arange: Error:", abs(SAMPLING_PERIOD - tb_sample_period))
-    
-    # Using numpy.arange is better
-    t = tb
 
-    # Piano note's frequencies in Hz
+    # 220 Hz    0.25 >= t < 1.00
+    # 440 Hz    0.50 >= t < 1.00
+    # 1760 Hz   0.50 >= t < 0.75
     FREQ_A3 = 220
     FREQ_A4 = 440
     FREQ_A6 = 1760
@@ -84,32 +67,11 @@ if __name__ == "__main__":
                        (50, 100, FREQ_A4),
                        (25, 75, FREQ_A6)]
 
-    # TODO revert
-    # t = np.arange(0, AUDIO_LENGTH_SECONDS*SAMPLING_FREQUENCY, AUDIO_LENGTH_SECONDS)
-    # t = np.arange(0, (8*SAMPLING_PERIOD), SAMPLING_PERIOD)
     t = np.arange(0, AUDIO_LENGTH_SECONDS, SAMPLING_PERIOD)
-    # t = np.arange(0, 0.8, 0.1)
 
-    signal = create_mixed_signal(t, list_of_signals, AUDIO_LENGTH_SECONDS, SAMPLING_FREQUENCY)
-
-    # 220 Hz 0 >= t < 2
-    signal_a3 = np.sin(2*np.pi*FREQ_A3*t)
-
-    # 440 Hz 1 >= t < 2
-    # t4 = np.pad(t, (50 / 100 *t.size, 100 / 100), 'constant')[(50 / 100)*t.size:(100 / 100)*t.size]
-    # signal_a4 = np.sin(2*np.pi*FREQ_A4*t4)
-
-    # 1760 Hz 0.5 >= t < 1.5
-    
-    # t4 = np.pad(t, (shift, 0), 'constant')[:t.size]
-    # signal_a4 = np.sin(2*np.pi*FREQ_A4*t4)
-
+    signal = create_mixed_signal(t, list_of_signals)
 
     fig, ax = plt.subplots()
-    # ax.spines['bottom'].set_position('bottom')
     ax.plot(t, signal)
     ax.grid()
     plt.show()
-
-    while(True):
-        x = 1
