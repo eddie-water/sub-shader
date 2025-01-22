866215ff6491a369dcabcab35efbd1538c05e353
Eddie Devlin <edevlin64@gmail.com>
2025-01-22 07:43:57 -0500

    Removed constants for passed in arguments for the sampling freq. Also cleaned up readability

--- wavelet.py
+++ wavelet.py
@@ -1,40 +1,42 @@
 import numpy as np
 import pywt
 
-# Signal Info
-SAMPLING_FREQUENCY = 44100
-NYQUIST_FREQUENCY = SAMPLING_FREQUENCY / 2
-SAMPLING_PERIOD = 1.0 / SAMPLING_FREQUENCY
-AUDIO_LENGTH_SECONDS = .1
+# Most audio is sampled 44.1 kHz
+TYPICAL_SAMPLING_FREQ = 44100
 
-# Calculate the set of scales we are interested in 
-VOICES_PER_OCTAVE = 12 # TODO maybe call this notes/ per octave 
+# Musical Scale parameters 
+NOTES_PER_OCTAVE = 12 
 NUM_OCTAVES = 10
 ROOT_NOTE_A0 = 27.5
 
 class Wavelet():
-    def __init__(self, frame_size: int):
-        """Configuration for the Continuous Wavelet Transform"""
+    def __init__(self, sampling_freq: int, frame_size: int):
+        if (sampling_freq != TYPICAL_SAMPLING_FREQ):
+            raise ValueError("Irregular Sampling Frequency")
+
+        # Sampling Info
+        self.sampling_freq = sampling_freq
+        self.nyquist_freq = (sampling_freq / 2.0)
+        self.sampling_period = (1.0 / self.sampling_freq)
         self.frame_size = frame_size
 
         # Wavelet info
         self.wavelet_name = "cmor1.5-1.0" # TODO LATER why 1.5-1.0?
-        self.sampling_period = SAMPLING_PERIOD
 
         # Configure time and frequency axes
         self.time = np.arange(0, frame_size) * self.sampling_period
 
-        # Frequency array that mimic the variable step size of musical notes
-        scale_factor = 2**(1/VOICES_PER_OCTAVE)
-        i = np.arange(0, VOICES_PER_OCTAVE*NUM_OCTAVES, 1)
+        # Frequency array that mimics the variable step size of musical notes
+        scale_factor = 2**(1/NOTES_PER_OCTAVE)
+        i = np.arange(0, NOTES_PER_OCTAVE*NUM_OCTAVES, 1)
         s = scale_factor**i
         self.freq = ROOT_NOTE_A0*s
 
         # Discard frequencies that are unmeasurable
-        self.freq = self.freq[self.freq < NYQUIST_FREQUENCY]
+        self.freq = self.freq[self.freq < self.nyquist_freq]
 
         # Scale array used to specify wavelet dilation amounts during cwt
-        f_norm = self.freq / SAMPLING_FREQUENCY # TODO add comment why we do this
+        f_norm = (self.freq / self.sampling_freq) # TODO comment why we do this
         self.scales = pywt.frequency2scale(self.wavelet_name, f_norm)
 
     def get_shape(self) -> np.ndarray.shape:
@@ -59,6 +61,9 @@ class Wavelet():
                                 wavelet= self.wavelet_name,
                                 sampling_period= self.sampling_period)
 
-        # Important step - take abs value of the data
+        # Important step - take abs value of the data and reduce axes by 1
         abs_coefs = np.abs(coefs[:-1, :-1])
+
+        # TODO here is where we would use a log scale to sharpen the colors
+
         return abs_coefs
