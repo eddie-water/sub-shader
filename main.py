ee2925eb0693238e5651f1462c95cd901c190b77
Eddie Devlin <edevlin64@gmail.com>
2025-01-23 06:48:11 -0500

    Using perfcounter to time the main loop

--- main.py
+++ main.py
@@ -6,66 +6,82 @@ from audio_input import AudioInput
 from wavelet import Wavelet
 from plotter import Plotter
 
-"""
-Constants
-    Ideally we have the biggest frame size and the smallest downsample possible
-"""
+import time
+
+# Constants: 
+#   Ideally have the biggest frame size and the smallest downsample factor 
 FRAME_SIZE = 256
 DOWNSAMPLE_FACTOR = 8
 
 FILE_PATH = "audio_files/zionsville.wav"
 
 """
-Audio Input, Audio Characteristics 
+Start Up
 """
+# Audio Input, Audio Characteristics 
 audio_input = AudioInput(path = FILE_PATH, frame_size = FRAME_SIZE)
 
 sampling_freq = audio_input.get_sample_rate() # 44.1 kHz
 sampling_period = (1.0 / sampling_freq)
 
-"""
-Wavelet Object
-"""
+# Wavelet Object
 wavelet = Wavelet(sampling_freq = sampling_freq, 
                   frame_size = FRAME_SIZE,
                   downsample_factor = DOWNSAMPLE_FACTOR)
 
 data_shape = wavelet.get_shape()
 
-"""
-Plotter Object
-"""
+# Plotter Object
 plotter = Plotter(file_path = FILE_PATH)
 
+# Loop Timing
+print("Loop Timing Analysis:")
+loop_count = 0
+
 """
-Main Loop
+Runtime Loop
 """
 def main_loop():
+    # Grab the global loop variable
+    global loop_count
+
     # Grab a frame of audio
+    t_start = time.perf_counter()
     audio_data = audio_input.get_frame()
+    t_audio = time.perf_counter() - t_start
 
     # Compute CWT on that frame
+    t_start = time.perf_counter()
     coefs = wavelet.compute_cwt(audio_data)
+    t_cwt = time.perf_counter() - t_start
 
     # Update plot
+    t_start = time.perf_counter()
     plotter.update_plot(coefs)
+    t_plot = time.perf_counter() - t_start
 
     # Update FPS Count
+    t_start = time.perf_counter()
     fps_counter.update()
+    t_fps = time.perf_counter() - t_start
+
+    # Print Timing Analysis
+    print("Runtime Loop:", loop_count)
+    print("Audio Get Frame:", t_audio)
+    print("Wavelet CWT    :", t_cwt)
+    print("Update Plot    :", t_plot)
+    print("Update FPS     :", t_fps)
+    print("\n")
+    loop_count += 1
 
 timer = QtCore.QTimer()
 timer.timeout.connect(main_loop)
 timer.start()
 
-"""
-FPS Counter
-    Stolen from PyQtGraph
-"""
+# FPS Counter (stolen from PyQtGraph)
 fps_counter = FrameCounter()
 fps_counter.sigFpsUpdate.connect(lambda fps: plotter.update_fps(fps))
 
-"""
-Main 
-"""
+# Main 
 if __name__ == '__main__':
     pg.exec()
