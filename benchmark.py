2180749984518b3ba7847d20a72b8a48b9a2834f
Eddie Devlin <edevlin64@gmail.com>
2025-01-28 06:24:38 -0500

    Removed all timing from main and cleaned up benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,10 +1,11 @@
 import time
+import numpy as np
 
 from audio_input import AudioInput
 from wavelet import Wavelet
 from plotter import Plotter 
 
-NUM_ITERATIONS = 10
+NUM_ITERATIONS = 100
 
 # TODO NEXT make a list of frame sizes and downsample factors to see
 # which combo gets the best performance
@@ -13,12 +14,6 @@ DOWNSAMPLE_FACTOR = 8
 
 # TODO NEXT Create a wav for bench testing
 FILE_PATH = "audio_files/zionsville.wav"
-
-# TODO SOON figure out a way to take the return value a method, and insert it
-# into the list for the next function
-
-# TODO SOON maybe that's not the point of bench test, maybe it should all
-# just be dummy values
 
 class Benchtest():
     def __init__(self) -> None:
@@ -39,36 +34,43 @@ class Benchtest():
         # Plotter Object
         plotter = Plotter(file_path = FILE_PATH)
 
-        # Function List
-        self.function_list = [
+        # Function List AN
+        self.func_list = [
             (audio_input.get_frame, ()),
             (wavelet.compute_cwt,   (dummy_audio,)),
             (plotter.update_plot,   (dummy_coefs,))
         ]
 
+        self.func_times = np.zeros(len(self.func_list))
+
     def main(self):
         print("Timing Analysis")
 
-        loop_count = 0
-
         for i in range(NUM_ITERATIONS):
-            print(f"Loop {loop_count}:")
+            # print(f"Loop {i}:")
 
-            for item in self.function_list:
+            for i, item in enumerate(self.func_list):
+                # Grab the function
                 func = item[0]
                 args = item[1] if len(item) > 1 else ()
                 kwargs = item[2] if len(item) > 2 else {}
 
+                # Time the function
                 t_start = time.perf_counter()
                 _ = func(*args, **kwargs)
                 t_end = time.perf_counter()
                 t_delta = t_end - t_start
 
-                print(f"{func.__name__}:")
-                print(f"-> {t_delta:6f} s")
+                self.func_times[i] += t_delta
 
-            loop_count += 1
-            print()
+        # Average the times
+        self.avg_func_times = self.func_times / int(NUM_ITERATIONS)
+
+        print(f"Time to run each function averaged at {NUM_ITERATIONS} times")
+
+        for i, item in enumerate(self.func_list):
+            func = item[0]
+            print(f"-> {func.__name__}\t{self.avg_func_times[i]:6f} sec")
 
 if __name__ == '__main__':
     benchtest = Benchtest()
--- main.py
+++ main.py
@@ -34,53 +34,21 @@ wavelet = Wavelet(sampling_freq = sampling_freq,
 # Plotter Object
 plotter = Plotter(file_path = FILE_PATH)
 
-# Print Start Up Timing Analysis
-t_start_up_delta = time.perf_counter() - t_start_up_start
-print("Start Up Timing Analysis:", t_start_up_delta)
-print('\n')
-
-"""
-Runtime Loop
-"""
-# Loop Timing
-print("Runtime Loop Timing Analysis:")
-loop_count = 0
-
 def main_loop():
     # Grab the global loop variable
     global loop_count
 
     # Grab a frame of audio
-    t_start = time.perf_counter()
     audio_data = audio_input.get_frame()
-    t_audio = time.perf_counter() - t_start
 
     # Compute CWT on that frame
-    t_start = time.perf_counter()
     coefs = wavelet.compute_cwt(audio_data)
-    t_cwt = time.perf_counter() - t_start
 
     # Update plot
-    t_start = time.perf_counter()
     plotter.update_plot(coefs)
-    t_plot = time.perf_counter() - t_start
 
     # Update FPS Count
-    t_start = time.perf_counter()
     fps_counter.update()
-    t_fps = time.perf_counter() - t_start
-
-    # Print Runtime Loop Timing Analysis
-    t_loop_total = t_audio + t_cwt + t_plot + t_fps
-    print("Loop", loop_count, ":", t_loop_total)
-
-    print("Audio :", t_audio)
-    print("CWT   :", t_cwt)
-    print("Plot  :", t_plot)
-    print("FPS   :", t_fps)
-    print("\n")
-
-    loop_count += 1
 
 timer = QtCore.QTimer()
 timer.timeout.connect(main_loop)
