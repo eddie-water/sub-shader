ed80f60deccdf1cc0273eb5c2b11ecb96180f1f0
Eddie <edevlin64@gmail.com>
2025-04-09 21:42:02 -0400

    save point I guess

--- cuda_wavelet_sand.py
+++ cuda_wavelet_sand.py
@@ -23,23 +23,24 @@ audio_data = audio_input.get_frame()
 sampling_freq = audio_input.get_sample_rate() # 44.1 kHz
 
 # PyWavelet
-wavelet = Wavelet(sampling_freq = sampling_freq, 
-                  frame_size = FRAME_SIZE,
-                  downsample_factor = DOWNSAMPLE_FACTOR)
-coefs = wavelet.compute_cwt(audio_data)
-coefs = np.transpose(coefs)
+wavelet_pywavelet = Wavelet(sampling_freq = sampling_freq, 
+                            frame_size = FRAME_SIZE,
+                            downsample_factor = DOWNSAMPLE_FACTOR)
+coefs_pywavelet = wavelet_pywavelet.compute_cwt(audio_data)
+coefs_pywavelet = np.transpose(coefs_pywavelet)
 
 # PyTorchWavelet
-dt = wavelet.get_sample_period()
-dj = wavelet.get_scale_factor()
-torch_wavelet = WaveletTransformTorch(dt = dt, dj = dj, unbias = False)
-torch_coefs = torch_wavelet.power(audio_data)
+dt = wavelet_pywavelet.get_sample_period()
+dj = wavelet_pywavelet.get_scale_factor()
+wavelet_torch = WaveletTransformTorch(dt = dt, dj = dj, unbias = False)
+coefs_torch = wavelet_torch.power(audio_data)
 
 # PyCuDwt (doesn't have CWT unforunately)
-W = Wavelets(audio_data, "db2", 3)
-W.forward()
-W.soft_threshold(10)
-W.inverse()
+wavelet_pcudwt = Wavelets(audio_data, "db2", 3)
+wavelet_pcudwt.forward()
+wavelet_pcudwt.soft_threshold(10)
+wavelet_pcudwt.inverse()
+coefs_pycudwt = wavelet_pcudwt.image
 
 # Plot
 fig, axes = plt.subplots(4, 1, figsize=(10,5))
@@ -49,15 +50,15 @@ axes[0].plot(audio_data)
 axes[0].axis('off')
 
 axes[1].set_title("PyWavelet")
-axes[1].imshow(coefs, cmap = "magma", aspect = "auto")
+axes[1].imshow(coefs_pywavelet, cmap = "magma", aspect = "auto")
 axes[1].axis('off')
 
 axes[2].set_title("PyTorchWavelet")
-axes[2].imshow(torch_coefs, cmap = "magma", aspect = "auto")
+axes[2].imshow(coefs_torch, cmap = "magma", aspect = "auto")
 axes[2].axis('off')
 
 axes[3].set_title("PyCuDwt")
-axes[3].imshow(W.image, cmap = "magma", aspect="auto")
+axes[3].imshow(coefs_pycudwt, cmap = "magma", aspect="auto")
 axes[3].axis('off')
 
 plt.show()
