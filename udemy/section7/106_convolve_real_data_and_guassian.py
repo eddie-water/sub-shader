1b0e28a6ed7597c383cbc1ed4b345a1eb9191a2e
Eddie <edevlin64@gmail.com>
2025-04-30 22:10:00 -0400

    Updated the variable names to make more sense to me

--- udemy/section7/106_convolve_real_data_and_guassian.py
+++ udemy/section7/106_convolve_real_data_and_guassian.py
@@ -5,45 +5,45 @@ import matplotlib.pyplot as plt
 
 data = loadmat("./ANTS_matlab/v1_laminar.mat")
 
-srate = data['srate']
-timevec = data['timevec'].T
+sample_rate = data['srate']
+t = data['timevec'].T
 
 # Signal will be ERP data from channel 7
 signal = np.mean(data['csd'][6, :, :], axis=1)
 
-# Create a Gaussian and normalize it
+# Create a Gaussian Curve and normalize it
 h = .05; # FWHM (seconds)
-gtime =  np.arange(-1,1,1/srate)
-gaus = np.exp( -4*np.log(2)*gtime**2 / h**2 )
-gaus = gaus/sum(gaus) 
+gaus_t =  np.arange(-1,1,1/sample_rate)
+gaus = np.exp(-4 * np.log(2) * gaus_t**2 / h**2 )
+gaus = gaus / sum(gaus) 
 
 # Step 1: N's of Convolution
-ndata = len(signal)
-nkern = len(gaus)
-nConv = ndata + nkern - 1 # convolution length
-halfK = nkern // 2
+signal_len = len(signal)
+kernel_len = len(gaus)
+conv_len = signal_len + kernel_len - 1 # convolution length
+half_kern_len = kernel_len // 2
 
 # Step 2: FFT the Signal and Kernel
-dataX = fft(signal, n=nConv)
-kernX = fft(gaus, n=nConv)
+signal_x = fft(signal, n=conv_len)
+kernel_x = fft(gaus, n=conv_len)
 
-# plt.plot(dataX)
-# plt.plot(kernX)
+# plt.plot(signal_x)
+# plt.plot(kernel_x)
 # plt.show()
 
 # Step 3: Multiply the Spectra
-convresX = dataX * kernX
+conv_x = signal_x * kernel_x
 
 # Step 4: IFFT the Spectrum back to Time Domain
-convres = ifft(convresX)
+conv = ifft(conv_x)
 
 # Step 5: Cut off the "Wings"
-convres = convres[halfK : -halfK + 1]
-plt.plot(timevec, signal, label='Original ERP')
-plt.plot(timevec, convres.real,'r',linewidth=2, label='Gaussian-convolved')
+conv = conv[half_kern_len : -half_kern_len + 1]
+plt.plot(t, signal, label='Original ERP')
+plt.plot(t, conv.real,'r',linewidth=2, label='Gaussian-convolved')
 
 plt.xlim([-.1, 1.4])
 plt.legend()
 plt.xlabel('Time (s)')
-plt.ylabel('Activity (\muV)')
+plt.ylabel('Activity (\muV)') # TODO SOON Fix deprecation error
 plt.show()
