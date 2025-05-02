96f91e4e99feb5a55a3105c257b21b455e967e47
Eddie <edevlin64@gmail.com>
2025-05-02 17:28:18 -0400

    Some things in the Matlab script I'm following get defined in the .mat file they load, which is impossible to see until I got a free trial for matlab and ran it myself. Top plot looks vbetter.

--- udemy/section7/110_cmw_convolution.py
+++ udemy/section7/110_cmw_convolution.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from numpy.fft import fft, ifft
 from scipy.io import loadmat
@@ -5,14 +6,14 @@ import matplotlib.pyplot as plt
 
 pi = np.pi
 
-file_data = loadmat("./ANTS_matlab/v1_laminar.mat")
+file_data = loadmat("./udemy/section7/ANTS_matlab/v1_laminar.mat")
 csd = file_data["csd"]
-data = csd[5,:,9]
-print("data shape =", data.shape)
+data = csd[5,:,9] 
 data = data.flatten()
-print("data shape flattened =", data.shape)
 
-sample_rate = 1000
+# Not part of the Matlab code, but the .mat file defines this when it gets 
+# loaded in Matlab the script. Need to do it explicitly myself 
+sample_rate = 762.9395
 
 # Create a centered time vector for the CMW
 cmw_t = np.arange(2*sample_rate) / sample_rate
@@ -25,22 +26,20 @@ freq = 45
 s = 7 / (2*pi*freq)
 
 # Create a Complex Morlet Wavelet
-cmw = np.exp(1j*2*pi*freq*cmw_t) * np.exp((-cmw_t**2) / ((2*s)**2))
+cmw = np.exp(1j*2*pi*freq*cmw_t) * np.exp((-cmw_t**2) / (2*s**2))
 
 # N's of Convolution
 t = file_data['timevec']
-print("t shape =", data.shape)
 t = t.flatten()
-print("t shape flattened =", data.shape)
 
 data_n = len(t)
 kern_n = len(cmw)
-conv_n = data_n + kern_n - 1 # convolution length
-half_kern_n = kern_n // 2
+conv_n = data_n + kern_n - 1    # convolution length
+half_kern_n = kern_n // 2       # this equivalent to floor(kern_n / 2)
 
 '''
 I like to use the '_x' suffix to signify that this variable represents frequency
-domain data / spectrum
+domain data / spectrum data
 '''
 
 # FFT the signal and the kernel, normalize the kernel
@@ -48,17 +47,47 @@ data_x = fft(data, conv_n)
 kern_x = fft(cmw, conv_n)
 kern_x = kern_x / max(kern_x)
 
-# Multiply the spectra, IFFT result back to the time domain and cut off wings
+# Need to plot these separately bc they have such a large magnitude discrepancy
+plt.plot(abs(data_x))
+plt.show()
+plt.show()
+
+plt.plot(abs(kern_x))
+plt.show()
+
+# Multiply the spectra
 conv_x = data_x * kern_x
+
+# These plots are off by a little: compare this plot to Lesson 110 @ 8:13 
+plt.plot(abs(data_x))
+plt.plot(abs(conv_x))
+plt.show()
+
+# IFFT result back to the time domain and cut off wings
 conv = ifft(conv_x)
 conv = conv[half_kern_n : -half_kern_n+1]
 
 # I don't trust this guy's x-axes... but here goes it
-hz = np.linspace(0, sample_rate / 2, (conv_n//2+1))
+hz = np.linspace(0, (sample_rate / 2), (conv_n//2) + 1)
 
-# Result of a convolution of a CMW centered at 45 Hz and 
-plt.plot(t, data, color = 'orange', label = "LFP data")
-plt.plot(t, abs(conv), color = 'black', linewidth = 2, label = 'Convolved Data')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude (uV)')
+fig, axes = plt.subplots(2)
+axes[0].plot(hz, abs(data_x[0:len(hz)]))
+axes[0].plot(hz, abs(kern_x[0:len(hz)]) * max(abs(data_x))/2)
+axes[0].plot(hz, abs(conv_x[0:len(hz)]), color = 'black', linewidth = 2)
+
+axes[0].set_title('Frequency Plot')
+axes[0].set_xlabel('Frequency (Hz)')
+axes[0].set_xlim(0, 90)
+axes[0].set_ylabel('Amplitude (A. U.)')
+axes[0].set_ylim(0, 3e5)
+
+axes[1].plot(t, data, color = 'orange', label = "LFP data")
+axes[1].plot(t, abs(conv), color = 'black', linewidth = 2, label = 'Convolved Data')
+axes[1].set_title('Time Plot')
+axes[1].set_xlabel('Time (ms)')
+axes[1].set_xlim(-0.1, 1.3)
+axes[1].set_ylabel('Amplitude (uV)')
+axes[1].set_ylim(-2000, 2000)
+
+plt.tight_layout()
 plt.show()
