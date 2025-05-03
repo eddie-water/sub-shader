99224cbe2db7e2718cbe35c8d9df4156345764e0
Eddie <edevlin64@gmail.com>
2025-05-03 19:45:33 -0400

    We're going to say that 111 follows the multitrial convolution like the lesson does, and 112 does the same except does it on a single trial. The multi-trial may be useful one day in the future, but don't really need it right now.

--- udemy/section7/112_time_frequency_power_plot.py
+++ udemy/section7/112_time_frequency_power_plot.py
@@ -1,15 +1,17 @@
 """
-Multi-Trial Convolutions
+Full Time-Frequency Power Plot
 
 Analyzing Neural Time Series (ANTS) by Mike X Cohen, Lessons 111 and 112
 
-This implements the Continuous Wavelet Transform on 200 trials of some example 
-neurosignal data. I'm not particularly interested in the multi-trial aspect, 
-but I do like how he re-shaped the data and performed the convolution. I think 
-it's a clever way of running the same convolution on multiple trials. Maybe 
-I'll have a need for something like this in the future, so I wanted to keep 
-track of it here.
+This script diverges from the lesson and example code becuase this script 
+implements the Continuous Wavelet Transform on a single trial of example 
+neurosignal data, rather than 200 trials. Regarding the application I'm working 
+towards, I won't have multiple trials to conduct multiple convolutional runs 
+on. I'm just going to have a signal that comes in and needs to be analyzed 
+accurately as fast as possible. So I don't see where running multiple trials of 
+anything will play a part in it. 
 """
+import os
 import numpy as np
 from numpy.fft import fft, ifft
 from scipy.io import loadmat
@@ -17,20 +19,17 @@ import matplotlib.pyplot as plt
 
 pi = np.pi
 
-'''
-Load the data from a file and prepare for the convolution
+"""
+Load the data and prepare for the convolutional loop
 
-Note: the sample rate needs to be defined explicitly here because in the video
+The sample rate needed to be defined explicitly here because in the video
 lesson, it is automatically defined when loading the .mat file in Matlab
-'''
-# Grab channel 5, all the time samples (1527), all the trials (200)
+"""
+# Grab channel 5, all the time samples (1527), and trial #100
 file_data = loadmat("./udemy/section7/ANTS_matlab/v1_laminar.mat")
 csd = file_data["csd"]
-data = np.squeeze(csd[5,:,:])
+data = np.squeeze(csd[5,:,100])
 data = data.astype(np.float64) # double precision
-
-# Reshape the data - TODO NEXT Why do we need to reshape it?
-data_r = np.reshape(data, -1, order='F')
 
 sample_rate = 762.9395
 
@@ -39,7 +38,7 @@ cmw_t = np.arange(2*sample_rate) / sample_rate
 cmw_t = cmw_t - np.mean(cmw_t) 
 
 # N's of convolution, note we're using the size of the reshaped data
-data_n = len(data_r)
+data_n = len(data)
 kern_n = len(cmw_t)
 conv_n = data_n + kern_n - 1
 half_kern_n = kern_n // 2
@@ -48,7 +47,7 @@ t = file_data['timevec']
 t = t.flatten()
 
 # Transform the Data time series into a spectrum
-data_x = fft(data_r, conv_n)
+data_x = fft(data, conv_n)
 
 # Frequency parameters in Hz
 min_freq = 5 
@@ -61,20 +60,19 @@ freqs = np.linspace(min_freq, max_freq, num_freq)
 # Initialize TF matrix
 tf = np.zeros((num_freq, len(t)))
 
-'''
-TODO NOW
+"""
+Convolutional Loop:
 
-Create a for loop that:
-Creates a wavelet centered around a particular frequency from our list of 
+Creates a wavelet centered around a particular frequency from the list of 
 frequencies. Transform the wavelet kernel into a spectrum and normalize it. 
-Multiply the data and wavelet spectra to produce a filtered output signal. 
-Transform back into the time domain, trim the wings and reshape back into the 
-original data shape. Extract the power of the filtered signal. Average the 
-power of all the trials and store it in a matrix to easily plot the '3D' result.
-'''
-
-# TODO SOON Figure out the significance of this parameter
-s = 0.3
+Multiply the data (that was already transformed before the loop) and wavelet 
+spectra to produce a filtered output signal (convolution result) back into the 
+time domain, trim the wings and reshape back into the original data shape. 
+Extract the power of the filtered signal. Average the power of all the trials 
+and store it in a matrix to easily plot in time-frequency plot.
+"""
+# Full-Width Half Maximum - try different out some different values
+s = 0.3 
 
 # TODO NEXT figure out why we create the wavelet on a different time vector
 for i in range(num_freq):
@@ -86,8 +84,7 @@ for i in range(num_freq):
     conv = ifft(data_x * cmw_x)
     conv = conv[(half_kern_n):(-half_kern_n+1)]
     conv_pow = abs(conv)**2
-    conv_pow = np.reshape(conv_pow, data.shape, order = 'F')
-    tf[i,:] = np.mean(conv_pow, 1)
+    tf[i,:] = conv_pow
 
 fig, ax = plt.subplots()
 
