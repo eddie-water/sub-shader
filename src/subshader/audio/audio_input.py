77a428b33af1c39f0d87bd5a87f483715f51d6fe
Eddie <edevlin64@gmail.com>
2025-06-19 19:58:12 -0400

    Docstrings for audio input, removed an unused function

--- src/subshader/audio/audio_input.py
+++ src/subshader/audio/audio_input.py
@@ -15,11 +15,13 @@ class AudioInput:
         self.overlap = OVERLAP / 100.0
         self.slide_amount = int(self.window_size * self.overlap)
 
-    """
-    Get Frame
-        Returns: a block of audio whose size is specified by the window size
-    """
     def get_frame(self) -> np.ndarray:
+        """
+        Gets a frame of audio the size of the window.
+
+        Returns:
+            np.ndarray: The next frame of audio data from the file.
+        """
         with sf.SoundFile(self.file_path, 'r') as f:
             # Read one frame's worth of audio sample
             f.seek(self.pos)
@@ -31,28 +33,20 @@ class AudioInput:
 
         return self.data
 
-    """
-    Get Entire Audio
-        Returns: the entire audio file
-    """
-    def get_entire_audio(self) -> np.ndarray:
-        with sf.SoundFile(self.file_path, 'r') as f:
-            self.entire_file_size = f.frames
-
-            f.seek(0)
-            self.data = f.read(self.entire_file_size)
-            self.data = self.data[:,0]
-            return self.data        
-
-    """
-    Get Sample Rate
-        Returns: the sample rate of the file
-    """
     def get_sample_rate(self) -> int:
+        """
+        Gets the sample rate of the audio file.
+
+        Returns:
+            int: Sample rate
+        """
         with sf.SoundFile(self.file_path, 'r') as f:
             return f.samplerate
 
     def _display_file_info(self) -> None:
+        """
+        Prints information about the audio file.
+        """
         with sf.SoundFile(self.file_path, 'r') as f:
             print("Information about the file:", self.file_path)
             print("mode", f.mode)
