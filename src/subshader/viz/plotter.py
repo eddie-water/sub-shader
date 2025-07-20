137dfca6689fc8722e2a8dd2099dbe010d528b9c
Eddie <edevlin64@gmail.com>
2025-07-19 20:12:17 -0400

    Retruned PyQtGrapher class just in case - helpful for debugging sometimes

--- src/subshader/viz/plotter.py
+++ src/subshader/viz/plotter.py
@@ -3,6 +3,7 @@ import numpy as np
 import moderngl
 import glfw
 import logging
+import pyqtgraph as pg
 
 # Set up clean logging - file only, no console spam
 logging.basicConfig(
@@ -305,10 +306,8 @@ class Shader(Plotter):
         self.scaling = AdaptiveScaling()
         
         logger.info("Shader system ready")
-        print()
         print("Visualizer started - logs in 'shader_debug.log'")
-        print()
-
+    
     def update_plot(self, values):
         """Main API: Feed in coefficients, get beautiful visualization"""
         
@@ -333,6 +332,10 @@ class Shader(Plotter):
         self.render_target.render()
         self.gl_context.swap_buffers()
     
+    def update_fps(self, fps: int):
+        """GPU visualizer doesn't need FPS display"""
+        pass
+    
     def should_window_close(self):
         """Check if user wants to close the window"""
         return self.gl_context.should_close()
@@ -340,106 +343,3 @@ class Shader(Plotter):
     def cleanup(self):
         """Clean shutdown"""
         glfw.terminate()
-
-class PyQtGrapher(Plotter):
-    def __init__(self, file_path: str, shape: tuple[int, int]):
-        super().__init__(file_path, shape)
-        """
-        Global Backend Config Options
-            useOpenGL           - enables OpenGL (seems to make things ~2x 
-                                  faster)
-            enableExperimental  - use PyOpenGL for curve drawing 
-
-        App, Widget, Plot
-        """
-        pg.setConfigOptions(useOpenGL = True, enableExperimental = True)
-
-        self.app = pg.mkQApp("Sub Shader")
-        self.win = pg.GraphicsLayoutWidget()
-        self.win.show()  
-        self.win.setWindowTitle('Continuous Wavelet Transform')
-
-        self.plot = self.win.addPlot(row = 0, 
-                                     col = 0, 
-                                     rowspan = 1,
-                                     colspan = 1,
-                                     title = file_path,
-                                     enableMenu = False)
-
-        """
-        Color Array and Plot
-
-        Color Array - The color values that fill the space between the points 
-                    in the mesh grid. Each dimension of c must be decreased by 
-                    1 to fit inside the bounds of the mesh
-
-        PColorMeshItem
-            colorMap    - color map ['viridis', 'plasma', 'inferno', 'magma', 
-                          'cividis']
-            edgeColors  - color of the edge of the polygons
-            antialias   - antialisasing when drawing edgelines if edgeColors == 
-                          True
-            levels      - min and max values of the color map
-            autoLevels  - autoscales the colormap levels when setData() is 
-                          called
-        """
-        colorMap        = pg.colormap.get('inferno')
-        edgeColors      = None
-        antialiasing    = False 
-        levels          = (-1, 1)
-        autoLevels      = False
-
-        # Autoscaling is performed just once, when color data is first supplied
-        self.pcolormesh = pg.PColorMeshItem(colorMap = colorMap,
-                                            levels = levels,
-                                            enableAutoLevels = autoLevels,
-                                            edgeColors = edgeColors,
-                                            antialiasing = antialiasing)
-
-        self.plot.addItem(self.pcolormesh)
-
-        """
-        Color Bar, Text Box
-
-        ColorBarItem
-            label       - title for color self.bar
-            interactive - True would override enableAutoLevels in PColorMesh
-            rounding    - decimal precision
-
-        TextItem
-            anchor  - x, y what corner of the text box anchors the text's 
-                      position
-            fill    - the background color fill
-        """
-        self.bar = pg.ColorBarItem(label = "Magnitude",
-                                   interactive = False,
-                                   limits = (0, 1),
-                                   rounding = 0.1)
-        self.bar.setImageItem([self.pcolormesh])
-        self.win.addItem(self.bar, 0, 1, 1, 1)
-
-        self.textBox = pg.TextItem(anchor = (0, 1),
-                                   fill = 'black')
-        self.textBox.setPos(1, 1)
-        self.plot.addItem(self.textBox)
-
-    """
-    Update Plot
-        Args:
-            values: coefficients to update the pcolormesh with
-    """
-    def update_plot(self, values):
-        # TODO ISSUE-33 Plotter Improvements fix axes so we don't have to transpose 
-        values = values.T
-        self.pcolormesh.setData(values)
-
-    """
-    Update FPS
-        Args:
-            fps: the value to update the FPS Textbox to
-    """
-    def update_fps(self, fps: int):
-        self.textBox.setText((f'{fps:.1f} fps'))
-
-    def should_window_close(self):
-        raise NotImplementedError("PyQtGraph-based window-check not implemented yet.")
